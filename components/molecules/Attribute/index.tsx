77cf64366a08a1cc0de306002d609005a4139da6
Sebastien Powell <sebastienpowell@gmail.com>
2021-02-18 12:16:14 +0000

    Fixes

--- components/molecules/Attribute/index.tsx
+++ components/molecules/Attribute/index.tsx
@@ -3,7 +3,7 @@ import {Box} from "components";
 export const Attribute = ({ label, value, py = 2, fontSize= "xs" }) => {
     return (
       <Box display="flex" py={py} alignItems="center" fontSize={fontSize}>
-        <Box  width="150px" lineHeight="none" color="gray.400" fontWeight={500}>{label}</Box>
+        <Box  width="150px" lineHeight="none" color="gray.400" fontWeight="semibold">{label}</Box>
         <Box> {value}</Box>
       </Box>
     );
