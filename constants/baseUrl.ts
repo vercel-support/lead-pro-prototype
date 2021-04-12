bdd1e6cad99dfdfd8d0d7de2412807aea59191d3
Sebastien Powell <sebastienpowell@gmail.com>
2021-04-12 08:09:20 +0100

    Fixed base url

--- constants/baseUrl.ts
+++ constants/baseUrl.ts
@@ -3,4 +3,4 @@ import { isDevelopment } from "utils/isDevelopment";
 export const BASE_URL =
 	isDevelopment === true
 		? "http://localhost:3000"
-		: "https://" + process.env.VERCEL_URL;
+		: "https://" + "lead-pro-prototype.vercel.app";
