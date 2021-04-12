95f3e2e11a22dbebd7fcef04121817a8319e00c6
Sebastien Powell <sebastienpowell@gmail.com>
2021-04-12 08:20:55 +0100

    Fixes

--- pages/api/compile.js
+++ pages/api/compile.js
@@ -1,14 +1,15 @@
 import mjml2html from "mjml";
 const fs = require("fs");
-const path = require("path");
+const path = require("path")
+const { join } = require('path')
 
 export default function handler(req, res) {
   if (req.method === "POST") {
     const { email } = JSON.parse(req.body);
-    const mjMail = fs.readFileSync(`../../emails/${email}.mjml`, "utf8");
+    const mjMail = fs.readFileSync(join(__dirname, `emails/${email}.mjml`), "utf8");
 
     const { html, errors } = mjml2html(mjMail, {
-      filePath: "../../emails",
+      filePath: "emails",
     });
 
     res.status(200).json({ html });
