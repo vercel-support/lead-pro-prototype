c4a84fc26e9709c4753f97e8e8c2791f6ae10d49
Sebastien Powell <sebastienpowell@gmail.com>
2021-04-12 10:02:46 +0100

    Fixes

--- pages/api/compile.js
+++ pages/api/compile.js
@@ -7,10 +7,10 @@ export default function handler(req, res) {
   if (req.method === "POST") {
     const { email } = JSON.parse(req.body);
     
-    const mjMail = fs.readFileSync(join(".", __dirname, "public/_files", `${email}.mjml`), "utf8");
+    const mjMail = fs.readFileSync(join(".", __dirname, "_files", `${email}.mjml`), "utf8");
     
     const { html, errors } = mjml2html(mjMail, {
-      filePath: "public/_files",
+      filePath: "_files",
     });
 
     res.status(200).json({ html });
